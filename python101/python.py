7c30d5c2476395021ab4f14aa945305c55beafbe
Sampson1992 <boamah.sampson34@gmail.com>
2023-05-18 07:36:54 +0000

    data type

---
+++ python101/python.py
@@ -0,0 +1,28 @@
+#print('hello world')
+#law = ('happy new year')
+#print(len(law))
+#txt = "The best things in life are free!"
+#print("sampson" in txt)
+#a = "Hello, World!"
+#print(a.lower())
+#c = 7j
+#print(type(c))
+#thislist = ["apple", "banana", "cherry"]
+#print(thislist)
+#print(thislist[0,])
+
+#thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#print(thislist[2:5])
+
+#thislist = ["apple", "banana", "cherry"]
+#thislist[1] = "blackcurrant"
+#print(thislist)
+
+#thislist = ["apple", "banana", "cherry"]
+#thislist.append("orange")
+#print(thislist)
+
+
+#thislist = ["apple", "banana", "cherry"]
+#thislist.clear()
+#print(thislist)
